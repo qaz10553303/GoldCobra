e83a90481aad45d45b08dec9eb9a48a794f92849
qaz10553303 <39097598+qaz10553303@users.noreply.github.com>
2018-05-20 18:30:11 -0400

    Add files via upload

---
+++ src/sounds.js
@@ -0,0 +1,13 @@
+var JumpSFX= new Audio('../sounds/Jump.wav');
+var PowerupSFX = new Audio('../sounds/Powerup.wav');
+var PowerupSFX1= new Audio('../sounds/Powerup1.wav');
+var ShieldSFX= new Audio('../sounds/Shield.wav');
+var ShootSFX= new Audio('../sounds/Shoot.wav');
+var LaserSFX= new Audio('../sounds/Laser_Shoot.wav');              //More SFX will be added here//
+var DeathSFX= new Audio('../sounds/Death.wav');
+var HurtSFX= new Audio('../sounds/Hit_Hurt.wav');
+var DashSFX= new Audio('../sounds/Dash.wav');
+var CoinSFX= new Audio('../sounds/Coin.wav');
+var ExplosionSFX= new Audio('../sounds/Explosion.wav');
+
+//You can call these SFX by using code like JumpSFX.play(); when needed//
